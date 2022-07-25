856936ddb5db44a166670d17bee35bd9bf3e1ec0
Premashish Ghosh <76865140+PGMSE@users.noreply.github.com>
2022-07-26 01:58:28 +0530

    Time: 214 ms (65.84%), Space: 75.3 MB (5.68%) - LeetHub

---
+++ 904-fruit-into-baskets/904-fruit-into-baskets.cpp
@@ -0,0 +1,39 @@
+class Solution
+{
+    public:
+        int totalFruit(vector<int> &fruits)
+        {
+            int i = 0, j = 0;
+            int n = fruits.size();
+            int mx = INT_MIN;
+
+            map<int, int> m;
+
+            while (j < n)
+            {
+                m[fruits[j]]++;
+
+                if (m.size() == 2)
+                {
+                    mx = max(mx,j - i + 1);
+                }
+                else if (m.size() > 2)
+                {
+                    while (m.size() > 2)
+                    {
+                        if (m.find(fruits[i])!=m.end())
+                        {
+                            m[fruits[i]]--;
+                            if(m[fruits[i]]==0){
+                                m.erase(fruits[i]);
+                            }
+                        }
+                        i++;
+                    }
+                }
+                j++;
+            }
+            if(mx==INT_MIN) mx=fruits.size();
+            return mx;
+        }
+};
